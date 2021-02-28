22d22fe91aaa2ea5ca021899803afe5e64e04eda
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-02-28 14:59:24 -0500

    recipes layout center

--- src/App.js
+++ src/App.js
@@ -1,79 +1,94 @@
-import './App.css';
-import { Switch, Route, Redirect} from 'react-router-dom'
-import { TransitionGroup, CSSTransition } from 'react-transition-group'
-import { EasybaseProvider } from "easybase-react"
-import {createStore} from'redux'
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { EasybaseProvider } from "easybase-react";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducer from './redux/reducer'
-import React, { Suspense, lazy } from 'react'
+import reducer from "./redux/reducer";
+import React, { Suspense, lazy } from "react";
 
-import ebconfig from './ebconfig'
-import NavBar from './components/navbar/navbar.js'
-import Footer from './components/footer/Footer.js'
-// import About from './pages/about/About'
-// import Home from "./pages/home/Home";
-// import Recipes from './pages/recipes/recipes'
-// import Card from './pages/card/card'
-import { BrowserRouter} from "react-router-dom";
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+import ebconfig from "./ebconfig";
+import NavBar from "./components/navbar/navbar.js";
+import Footer from "./components/footer/Footer.js";
+import { BrowserRouter } from "react-router-dom";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import { PersistGate } from "redux-persist/lib/integration/react";
 
-
-const Home = lazy(() => import('./pages/home/Home'));
-const About = lazy(() => import('./pages/about/About'));
-const Recipes = lazy(() => import('./pages/recipes/recipes'));
-const Card = lazy(() => import('./pages/card/card'));
+const Home = lazy(() => import("./pages/home/Home"));
+const About = lazy(() => import("./pages/about/About"));
+const Recipes = lazy(() => import("./pages/recipes/recipes"));
+const Card = lazy(() => import("./pages/card/card"));
 
 function App() {
   const persistConfig = {
-    key: 'root',
+    key: "root",
     storage: storage,
-    stateReconciler: autoMergeLevel2 // see "Merge Process" section for details.
-   };
-  
-const pReducer = persistReducer(persistConfig, reducer);
-const store = createStore(pReducer);
-const persistor = persistStore(store);
+    stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+  };
+
+  const pReducer = persistReducer(persistConfig, reducer);
+  const store = createStore(pReducer);
+  const persistor = persistStore(store);
 
   return (
-    <div className="App" style={{ position: "relative"}}>
+    <div className="App" style={{ position: "relative" }}>
       <Provider store={store}>
-        <PersistGate  persistor={persistor}>
+        <PersistGate persistor={persistor}>
           <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <NavBar />
-            <Route render={({location}) => (
-              <EasybaseProvider ebconfig={ebconfig}>
-                  <TransitionGroup component={null}>
-                    <CSSTransition
-                      key={location.key}
-                      in = {true}
-                      timeout={500}
-                      classNames="fade"
-                    >
-                      <Switch>
-                          
-                            <Route exact path="/Home" className="Home" component={Home} />
-                            <Route exact path="/About" className="About" component={About} />
-                            <Route exact path="/Recipes" className="Recipes" component={Recipes} />
-                            <Route exact path="/Card/:name" className="Card" component={Card} />
-                          
-                            <Route exact path="/">
-                              <Redirect to="/Home" />
-                            </Route>
-                   
-                      </Switch>
-                    </CSSTransition>
-                  </TransitionGroup>
-                </EasybaseProvider>
-              )} />
-              </Suspense>
-            </BrowserRouter>
-          </PersistGate>
-        </Provider>
-        
+            <Suspense fallback={<div>Loading...</div>}>
+              <NavBar />
+              <Route
+                render={({ location }) => (
+                  <EasybaseProvider ebconfig={ebconfig}>
+                    <TransitionGroup component={null}>
+                      <CSSTransition
+                        key={location.key}
+                        in={true}
+                        timeout={500}
+                        classNames="fade"
+                      >
+                        <Switch>
+                          <Route
+                            exact
+                            path="/Home"
+                            className="Home"
+                            component={Home}
+                          />
+                          <Route
+                            exact
+                            path="/About"
+                            className="About"
+                            component={About}
+                          />
+                          <Route
+                            exact
+                            path="/Recipes"
+                            className="Recipes"
+                            component={Recipes}
+                          />
+                          <Route
+                            exact
+                            path="/Card/:name"
+                            className="Card"
+                            component={Card}
+                          />
+
+                          <Route exact path="/">
+                            <Redirect to="/Home" />
+                          </Route>
+                        </Switch>
+                      </CSSTransition>
+                    </TransitionGroup>
+                  </EasybaseProvider>
+                )}
+              />
+            </Suspense>
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+
       <Footer />
     </div>
   );
--- src/pages/recipes/recipes.js
+++ src/pages/recipes/recipes.js
@@ -1,89 +1,112 @@
-import React, { useEffect } from 'react'
-import { useEasybase } from "easybase-react"
-import { Container, Row, Col , Card} from 'react-bootstrap';
-import { Link } from 'react-router-dom'
-import {connect} from 'react-redux'
-import { SAVE_ALL_RECIPES } from '../../redux/actions';
-import './recipes.css'
+import React, { useEffect } from "react";
+import { useEasybase } from "easybase-react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { SAVE_ALL_RECIPES } from "../../redux/actions";
+import "./recipes.css";
 
+function Recipes({ save, recipes }) {
+  const { Frame, configureFrame, sync } = useEasybase();
 
-function Recipes({save, recipes}) {
-    const {
-        Frame, 
-        configureFrame, 
-        sync
-    } = useEasybase();
+  useEffect(() => {
+    configureFrame({ limit: 20, offset: 0 });
+    sync();
+    // save(SAVE_ALL_RECIPES, Frame());
+  });
 
-    useEffect(() => {
-       configureFrame({ limit: 20, offset: 0});
-       sync();
-    //   save(SAVE_ALL_RECIPES, Frame());
-    })
-    
-
-    return (
-        <>
-        <div >
-            <img className="d-block w-100 custom-img" src="https://images.unsplash.com/photo-1513104806186-1b00f3e7a65e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"></img>
-            <div className="downArrow bounce">
-                <i style={{color:"white"}}className="fas fa-arrow-down fa-3x"></i>  
-            </div>
+  return (
+    <>
+      <div>
+        <img
+          className="d-block w-100 custom-img"
+          src="https://images.unsplash.com/photo-1513104806186-1b00f3e7a65e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+        ></img>
+        <div className="downArrow bounce">
+          <i style={{ color: "white" }} className="fas fa-arrow-down fa-3x"></i>
         </div>
-        <div className="sub-head" style={{}} >
-           Something that is made with love, will always taste great!
+      </div>
+      <div className="sub-head" style={{}}>
+        Something that is made with love, will always taste great!
+      </div>
+
+      <hr style={{ width: "50%", height: "3rem" }} />
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "5em",
+        }}
+      >
+        <div style={{ backgroundColor: "white", width: "100%" }}>
+          <p
+            style={{
+              fontSize: "30px",
+              textAlign: "center",
+              fontFamily: "Snell Roundhand, cursive",
+              padding: "2rem",
+            }}
+          >
+            <strong>Recipes</strong>
+          </p>
+
+          <Container>
+            <Row className="pb-5 ">
+              {Frame().map((ele, i) => (
+                <Col
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Card
+                    style={{
+                      width: "17rem",
+                      borderRadius: "3rem",
+                      marginTop: "1rem",
+                    }}
+                  >
+                    <Card.Img
+                      style={{
+                        height: "15rem",
+                        borderRadius: "2rem",
+                        objectFit: "cover",
+                      }}
+                      src={ele.image}
+                    />
+                    <Card.Body>
+                      <Card.Title
+                        style={{ textDecoration: "underline overline" }}
+                      >
+                        {ele.name}
+                      </Card.Title>
+                      <Card.Text>{ele.subdescription}</Card.Text>
+                      <Link
+                        to={`/card/${ele.name}`}
+                        className="learn-more-button"
+                      >
+                        view <i class="fas fa-utensils"></i>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
         </div>
-
-        <hr style={{width:"50%", height:"3rem"}}/>
-
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center", 
-            paddingTop:"5em",
-            }}>
-                
-            <div style={{backgroundColor:"white", width:"100%"}}>
-            {/* <p style={{ fontSize:"30px",textAlign:"center", fontFamily:"Snell Roundhand, cursive", padding:"2rem"}}>
-                <strong>Recipes</strong>
-            </p> */}
-
-            <Container >
-                    <Row className="pl-4" >
-                        {Frame().map((ele, i) => 
-                            <Col sm={4} md={6} lg={4}>
-                                <Card style={{ width: '17rem', borderRadius:"3rem", marginTop:"1rem" }}>
-                                <Card.Img style={{height:"15rem", borderRadius:"2rem", objectFit:"cover"}} src={ele.image} />
-                                <Card.Body >
-                                    <Card.Title style={{textDecoration: "underline overline"}}>{ele.name}</Card.Title>
-                                    <Card.Text>
-                                    {ele.subdescription}
-                                    </Card.Text>
-                                    <Link to={`/card/${ele.name}`}
-                                      
-                                        className='learn-more-button'
-                                    >
-                                        view <i class="fas fa-utensils"></i>
-                                    </Link>
-                                </Card.Body>
-                                </Card>
-                            </Col >
-                        )}
-                    </Row>
-            </Container>
-            </div>
-        </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
-
-const mapStateToProps = (state) => (
-{
-    recipes: state.recipes
+const mapStateToProps = (state) => ({
+  recipes: state.recipes,
 });
 const mapDispatchToProps = (dispatch) => {
-    return {
-        save: (type,data) => dispatch({ type:type, payload:data}),
-    }
-}
+  return {
+    save: (type, data) => dispatch({ type: type, payload: data }),
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(Recipes);
