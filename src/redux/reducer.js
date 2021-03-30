1732194404eb6e755f07ae648fe9af695ba32bfc
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-03-30 11:51:39 -0400

    DEV: switched from using redux to using context API.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,25 +1,44 @@
-import { GET_ALL_RECIPES, GET_A_RECIPE, SAVE_ALL_RECIPES } from "./actions";
+// import { GET_ALL_RECIPES, GET_A_RECIPE, SAVE_ALL_RECIPES } from "./actions";
 
-const initialState = {
-  recipes: [1, 2],
+// const initialState = {
+//   recipes: [1, 2],
+// };
+
+// export default function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     case GET_ALL_RECIPES: {
+//       //console.log("I AM RETURNING ALL RECIPES!!!!!")
+//       return state;
+//     }
+
+//     case SAVE_ALL_RECIPES: {
+//       return {
+//         recipes: action.payload,
+//       };
+//     }
+//     case GET_A_RECIPE: {
+//       return state.recipes.find((recipe) => recipe.name === action.payload);
+//     }
+//     default:
+//       return state;
+//   }
+// }
+
+export const initialState = {
+  articles: [],
 };
 
-export default function reducer(state = initialState, action) {
+const reducer = (state, action) => {
   switch (action.type) {
-    case GET_ALL_RECIPES: {
-      //console.log("I AM RETURNING ALL RECIPES!!!!!")
-      return state;
-    }
-
-    case SAVE_ALL_RECIPES: {
+    case "SAVE_ARTICLES":
       return {
-        recipes: action.payload,
+        ...state,
+        articles: action.payload,
       };
-    }
-    case GET_A_RECIPE: {
-      return state.recipes.find((recipe) => recipe.name === action.payload);
-    }
+
     default:
       return state;
   }
-}
+};
+
+export default reducer;
